b6e904159607cd0074086bfe1965c0402bb32d2f
contrub <572519065z@gmail.com>
2022-07-05 15:08:12 +0300

    created API requester component

---
+++ CurrencyConverter/Services/APIRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+
+namespace CurrencyConverter
+{
+    class APIRequest
+    {
+        private static string NBURequestToCurrentDay = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+        private static string PrivatRequestToDate = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
+
+        private WebRequest request;
+        private WebResponse response;
+
+        public void MakeRequestToCurrentDay()
+        {
+            request = WebRequest.Create(NBURequestToCurrentDay);
+        }
+        public void MakeRequestToCurrentDatePrivat()
+        {
+            request = WebRequest.Create(PrivatRequestToDate);
+        }
+        public WebResponse GetResponse()
+        {
+            response = request.GetResponse();
+            return response;
+        }
+    }
+}
