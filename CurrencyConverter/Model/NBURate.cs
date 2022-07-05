07c5dd15135428789eb3887728aba093cacd29cf
contrub <572519065z@gmail.com>
2022-07-05 15:07:37 +0300

    updated parser models

--- CurrencyConverter/Model/NBURate.cs
+++ CurrencyConverter/Model/NBURate.cs
@@ -1,24 +1,23 @@
 ﻿using Newtonsoft.Json;
 
-namespace CurrencyConverter.Model.Parser
+namespace CurrencyConverter.Model
 {
     [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
-
     public class NBURate
     {
         [JsonProperty(PropertyName = "r030")]
-        public int r030 { get; set; }
+        public string r030 { get; set; }
 
         [JsonProperty(PropertyName = "txt")]
         public string txt { get; set; }
 
         [JsonProperty(PropertyName = "rate")]
-        public float rate { get; set; }
+        public double rate { get; set; }
 
         [JsonProperty(PropertyName = "cc")]
         public string cc { get; set; }
 
-        [JsonProperty(PropertyName = "exchangedate ")]
+        [JsonProperty(PropertyName = "exchangedate")]
         public string exchangedate { get; set; }
     }
 }
---
+++ CurrencyConverter/Model/PrivatBankRate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace CurrencyConverter.Model
+{
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    class PrivatBankRate
+    {
+        [JsonProperty(PropertyName = "ccy")]
+        public string ccy { get; set; }
+
+        [JsonProperty(PropertyName = "base_ccy")]
+        public string base_ccy { get; set; }
+
+        [JsonProperty(PropertyName = "buy")]
+        public double buy { get; set; }
+
+        [JsonProperty(PropertyName = "sale")]
+        public double sale { get; set; }
+    }
+}
