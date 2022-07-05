42a49e9f0844d045a3fe28f379be0710407eb522
contrub <572519065z@gmail.com>
2022-07-05 09:06:36 +0300

    created parser models

---
+++ CurrencyConverter/Model/Parser/NBURate.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace CurrencyConverter.Model.Parser
+{
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+
+    public class NBURate
+    {
+        [JsonProperty(PropertyName = "r030")]
+        public int r030 { get; set; }
+
+        [JsonProperty(PropertyName = "txt")]
+        public string txt { get; set; }
+
+        [JsonProperty(PropertyName = "rate")]
+        public float rate { get; set; }
+
+        [JsonProperty(PropertyName = "cc")]
+        public string cc { get; set; }
+
+        [JsonProperty(PropertyName = "exchangedate ")]
+        public string exchangedate { get; set; }
+    }
+}
