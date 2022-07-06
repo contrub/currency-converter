c828101f107947b0574278e911bf5e2cbaa03472
contrub <572519065z@gmail.com>
2022-07-06 18:44:23 +0300

    create controllers

---
+++ CurrencyConverter/Controller/ConverterController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CurrencyConverter.Model;
+
+namespace CurrencyConverter.Controller
+{
+    class ConverterController
+    {
+        public double NBUConvert(string baseCurrency, string convertedCurrency, double amount)
+        {
+            ParserController parser = new ParserController();
+
+            parser.FillCurrencyRateNbu();
+
+            if (baseCurrency.Equals("UAH") || convertedCurrency.Equals("UAH"))
+            {
+                return parser.GetCurrencyRateNBU(baseCurrency, convertedCurrency) * amount;
+            }
+            
+            return parser.GetCurrencyCrossRateNBU(baseCurrency, convertedCurrency) * amount;
+        }
+
+        public double PrivatBankConvert(string baseCurrency, string convertedCurrency, double inp)
+        {
+            ParserController parser = new ParserController();
+
+            parser.FillCurrencyRatePrivat();
+
+            if (baseCurrency.Equals("UAH") || convertedCurrency.Equals("UAH"))
+            {
+                return parser.GetCurrencyRatePrivat(baseCurrency, convertedCurrency) * inp;
+            }
+            
+            return parser.GetCurrencyCrossRatePrivat(baseCurrency, convertedCurrency) * inp;
+        }
+    }
+}
---
+++ CurrencyConverter/Controller/CurrencyController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter.Controller
+{
+    class CurrencyController
+    {
+        private MainWindow window;
+        private string baseCurrency;
+        private string convertedCurrency;
+        private double amountEntered;
+        
+        public CurrencyController(MainWindow newWindow)
+        {
+            window = newWindow;
+        }
+
+        public void UpdateValues()
+        {
+            baseCurrency = window.ExchangeFrom.Text;
+            convertedCurrency = window.ExchangeTo.Text;
+            amountEntered = double.Parse(window.Amount_Entered.Text);
+        }
+
+        public bool CheckCurrencies()
+        {
+            if (string.IsNullOrEmpty(window.ExchangeFrom.Text))
+            {
+                MessageBox.Show("You have not selected a currency to convert from");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(window.ExchangeTo.Text))
+            {
+                MessageBox.Show("You have not selected a currency to convert to");
+                return false;
+            }
+
+            try
+            {
+                double.Parse(window.Amount_Entered.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Error Input !");
+                return false;
+            }
+
+            if (double.Parse(window.Amount_Entered.Text) < 0)
+            {
+                MessageBox.Show("Cannot be negative number !");
+                return false;
+            }
+
+            return true;
+        }
+
+        public string GetBaseCurrency()
+        {
+            return baseCurrency;
+        }
+        public string GetConvertedCurrency()
+        {
+            return convertedCurrency;
+        }
+        public double GetAmountEntered()
+        {
+            return amountEntered;
+        }
+
+        public void SetAmountResult(double amount)
+        {
+            window.Amount_Show.Text = Math.Round(amount, 3).ToString();
+        }
+        public bool IsNbuChosen()
+        {
+            return (bool)window.checkBox1.IsChecked;
+        }
+        public bool IsPrivatBankChosen()
+        {
+            return (bool)window.checkBox2.IsChecked;
+        }
+
+        public void MakeConvert()
+        {
+            double convertedAmount = 1;
+
+            ConverterController convertCont = new ConverterController();
+
+            if (IsNbuChosen())
+            {
+                convertedAmount = convertCont.NBUConvert(
+                    baseCurrency.Substring(0, 3), convertedCurrency.Substring(0, 3), 
+                    amountEntered
+                    );
+            } else if (IsPrivatBankChosen())
+            {
+                convertedAmount = convertCont.PrivatBankConvert(
+                    baseCurrency.Substring(0, 3), convertedCurrency.Substring(0, 3),
+                    amountEntered
+                    );
+            }
+
+            SetAmountResult(convertedAmount);
+        }
+    }
+}
