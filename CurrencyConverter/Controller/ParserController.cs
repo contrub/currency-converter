bf4bae94beeb6c9a8818f79852ba4bf3fb9afc83
contrub <572519065z@gmail.com>
2022-07-06 18:44:39 +0300

    added get methods

--- CurrencyConverter/Controller/ParserController.cs
+++ CurrencyConverter/Controller/ParserController.cs
@@ -7,15 +7,15 @@ using CurrencyConverter.Model;
 
 namespace CurrencyConverter
 {
-    public class ParserController
+    class ParserController
     {
-        private string jsonResponse;
-        private List<NBURate> currencyRatesNBU;
+        private List<NBURate> currencyRateNbu;
         private List<PrivatBankRate> currencyRatePrivat;
 
-        public void FillListFromJSONToCurrentDayNBU()
+        public void FillCurrencyRateNbu()
         {
             ApiController API = new ApiController();
+            string jsonResponse;
 
             API.MakeRequestToCurrentDay();
 
@@ -27,11 +27,14 @@ namespace CurrencyConverter
                     jsonResponse = streamReader.ReadToEnd();
                 }
             }
-            currencyRatesNBU = JsonConvert.DeserializeObject<List<NBURate>>(jsonResponse);
+
+            currencyRateNbu = JsonConvert.DeserializeObject<List<NBURate>>(jsonResponse);
         }
-        public void FillCurrencyRateToCurrentDayPrivat()
+
+        public void FillCurrencyRatePrivat()
         {
             ApiController API = new ApiController();
+            string jsonResponse;
 
             API.MakeRequestToCurrentDatePrivat();
 
@@ -43,17 +46,124 @@ namespace CurrencyConverter
                     jsonResponse = streamReader.ReadToEnd();
                 }
             }
+
             currencyRatePrivat = JsonConvert.DeserializeObject<List<PrivatBankRate>>(jsonResponse);
         }
 
-        private List<NBURate> GetCurrencyRatesNBU()
+        public List<NBURate> GetCurrencyRatesNbu()
         {
-            return currencyRatesNBU;
+            return currencyRateNbu;
         }
 
-        private List<PrivatBankRate> GetCurrencyRatePrivat()
+        public List<PrivatBankRate> GetCurrencyRatesPrivat()
         {
             return currencyRatePrivat;
+        }
+
+        public double GetCurrencyRateNBU(string baseCurrency, string convertedCurrency)
+        {
+            double curerncyRate = 1;
+
+            if (baseCurrency.Equals(convertedCurrency))
+            {
+                return curerncyRate;
+            }
+
+            foreach (NBURate rate in currencyRateNbu)
+            {
+                if (rate.cc == baseCurrency)
+                {
+                    curerncyRate = rate.rate;
+                    break;
+                } 
+                else if (rate.cc == convertedCurrency)
+                {
+                    curerncyRate = 1 / rate.rate;
+                    break;
+                }
+            }
+
+            return curerncyRate;
+        }
+
+        public double GetCurrencyCrossRateNBU(string baseCurrency, string currency_to)
+        {
+            double result = 1;
+            double c1 = 1;
+            double c2 = 1;
+
+            if (baseCurrency.Equals(currency_to))
+            {
+                return result;
+            }
+
+            foreach (NBURate rate in currencyRateNbu)
+            {
+                if (rate.cc == baseCurrency)
+                {
+                    c1 = rate.rate;
+                } else if (rate.cc == currency_to)
+                {
+                    c2 = rate.rate;
+                }
+            }
+
+            result = c1 / c2;
+
+            return result;
+        }
+
+        public double GetCurrencyRatePrivat(string baseCurrency, string convertedCurrency)
+        {
+            double result = 1;
+
+            if (baseCurrency.Equals(convertedCurrency))
+            {
+                return result;
+            }
+
+            foreach (PrivatBankRate rate in currencyRatePrivat)
+            {
+                if (rate.ccy == baseCurrency)
+                {
+                    result = rate.buy;
+                    break;
+                } else if (rate.ccy == convertedCurrency)
+                {
+                    result = 1 / rate.sale;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public double GetCurrencyCrossRatePrivat(string baseCurrency, string convertedCurrency)
+        {
+            double result = 1; 
+            double c1 = 1;
+            double c2 = 1;
+
+            if (baseCurrency.Equals(convertedCurrency))
+            {
+                return result;
+            }
+
+            foreach (PrivatBankRate rate in currencyRatePrivat)
+            {
+                if (rate.ccy == baseCurrency)
+                {
+                    c1 = rate.buy;
+                }
+                else if (rate.ccy == convertedCurrency)
+                {
+                    c2 = rate.sale;
+                }
+            }
+
+            result = c1 / c2;
+
+            return result;
         }
     }
 }
