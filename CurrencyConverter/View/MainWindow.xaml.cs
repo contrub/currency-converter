ce8d481c001366946a936b21c822482e550dd3e7
contrub <572519065z@gmail.com>
2022-07-06 18:43:43 +0300

    gui minor update

--- CurrencyConverter/View/MainWindow.xaml.cs
+++ CurrencyConverter/View/MainWindow.xaml.cs
@@ -1,16 +1,25 @@
-﻿using CurrencyConverter.Model;
-using System;
+﻿using System;
 using System.Windows;
+using CurrencyConverter.Controller;
+
 namespace CurrencyConverter
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private CurrencyController currencyController;
         public MainWindow()
         {
             InitializeComponent();
+            currencyController = new CurrencyController(this);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {            
+            if (!currencyController.CheckCurrencies()) return;
+
+            currencyController.UpdateValues();
+
+            currencyController.MakeConvert();
         }
     }
 }
